4d25445857c28940b5bc53548a7da420aabb5be3
PanduBakti <pandu.bakti23@gmail.com>
2024-11-30 12:34:32 +0700

    title registrasi

--- register.php
+++ register.php
@@ -20,7 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registrasi - SmartGuard</title>
+    <title>Registrasi</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="register.css">
 </head>
