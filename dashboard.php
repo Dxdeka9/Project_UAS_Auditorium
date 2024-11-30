79f05c8703be63c90611a270daf2c082ed42eac9
sekarpn <sekarrpn04@gmail.com>
2024-11-30 15:57:08 +0700

    hapus file yg abduh coba

--- dashboard.php
+++ dashboard.php
@@ -69,9 +69,6 @@ $result = $stmt->get_result();
     </div>
 </div>
 
-<div>
-  cona aja dulu
-</div>
 
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
