7771c5b0e85de5194dff5ef719cf5563c797ed02
Deka Aditra Erlangga <150640353+Dxdeka9@users.noreply.github.com>
2024-12-01 11:28:27 +0700

    cookie di logout logout.php

--- logout.php
+++ logout.php
@@ -1,8 +1,6 @@
 <?php
     session_start();
     session_destroy();
-    header("Location: index.php");
-    exit();
 ?>
 
 <!DOCTYPE html>
@@ -10,7 +8,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cookie</title>
+    <title>Cookie logout</title>
 </head>
 <body>
     <script src="cookie.js"></script>
