54917f514f85feed2ddaf026d1bd7475a10bcae6
Deka Aditra Erlangga <150640353+Dxdeka9@users.noreply.github.com>
2024-12-04 08:28:16 +0700

    komen code buat jaga-jaga slideshow.js

--- slideshow.js
+++ slideshow.js
@@ -12,3 +12,21 @@ function showSlides() {
     slides[slideIndex - 1].style.display = "block";
     setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
+
+/* ini code buat jaga-jaga klo slideshow register.css ngawur
+let slideIndex = 0;
+const slides = document.querySelectorAll(".mySlides");
+
+function showSlides() {
+  slides.forEach((slide, index) => {
+    slide.classList.remove("active");
+  });
+  
+  slides[slideIndex].classList.add("active");
+  slideIndex = (slideIndex + 1) % slides.length;
+
+  setTimeout(showSlides, 3000); // Ubah slide setiap 3 detik
+}
+
+showSlides();
+*/
