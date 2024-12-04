6f3ed5c283b5cee517257a73eb733a6954ad2044
sekarpn <sekarrpn04@gmail.com>
2024-12-04 09:14:14 +0700

    ubah phpnya

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -2,7 +2,7 @@
     include 'db.php';
     session_start();
     // if (!isset($_SESSION['user_id'])) {
-    //     header("Location: index.php");
+    //     header("Location: login.php");
     //     exit();
     // }
     $user_id = $_SESSION['user_id'];
