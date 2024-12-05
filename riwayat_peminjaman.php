ce9fa89147aaa0ee459021ee9b40d2bd45263a96
AbduhRevan <abduhrevan40904@gmail.com>
2024-12-05 20:02:50 +0700

    nambahin button logout di sidebar admin

--- riwayat_peminjaman.php
+++ riwayat_peminjaman.php
@@ -43,9 +43,9 @@
                 <ul class="nav flex-column mb-auto">
                     <li class="nav-item"><a href="dashboard_admin.php" class="nav-link text-white">Dashboard</a></li>
                     <li class="nav-item"><a href="daftar_admin.php" class="nav-link text-white">Daftar Auditorium</a></li>
-                    <li class="nav-item"><a href="riwayat_peminjaman.php" class="nav-link">Riwayat Peminjaman</a></li>
+                    <li class="nav-item"><a href="riwayat_peminjaman.php" class="nav-link mb-4">Riwayat Peminjaman</a></li>
+                         <a href="logout.php" class="btn btn-danger w-100 mt-auto">Log out</a>
                 </ul>
-                <a href="logout.php" class="btn btn-danger w-100 mt-auto">Log out</a>
             </div>
     </div>
     </div>
