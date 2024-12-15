1805ab07941bbfacbcb0b02d724298f732a380cc
Deka Aditra Erlangga <150640353+Dxdeka9@users.noreply.github.com>
2024-12-15 13:01:03 +0700

    nambahin komentar kuki

--- cookie.js
+++ cookie.js
@@ -1,18 +1,18 @@
 // Fungsi untuk menyimpan cookie
 function setCookie(nama, value, hari = 30) {
-    const date = new Date();
-    date.setTime(date.getTime() + hari * 24 * 60 * 60 * 1000);
-    const expires = "expires=" + date.toUTCString();
-    document.cookie = `${nama}=${value}; ${expires}; path=/`;
+    const date = new Date(); // membuat objek tanggal
+    date.setTime(date.getTime() + hari * 24 * 60 * 60 * 1000); // menambah jumlah hari ke waktu sekarang
+    const expires = "expires=" + date.toUTCString(); // konversi expired ke format UTC (gmt)
+    document.cookie = `${nama}=${value}; ${expires}; path=/`; // menyimpan kuki
 }
 
 // Fungsi untuk membaca cookie
 function getCookie(nama) {
-    const cookies = document.cookie.split(';');
-    for (let i = 0; i < cookies.length; i++) {
+    const cookies = document.cookie.split(';'); // mengambil semua kuki ke bentuk string
+    for (let i = 0; i < cookies.length; i++) { // mencari kuki 
         const c = cookies[i].trim();
-        if (c.indexOf(nama + "=") === 0) {
-            return c.substring((nama + "=").length, c.length);
+        if (c.indexOf(nama + "=") === 0) { // cek nama kuki apakah sesuai
+            return c.substring((nama + "=").length, c.length); // nilai kuki
         }
     }
     return null;
@@ -20,5 +20,5 @@ function getCookie(nama) {
 
 // Fungsi untuk menghapus cookie
 function deleteCookie(nama) {
-    document.cookie = `${nama}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+    document.cookie = `${nama}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`; // mengatur kuki dengan tanggal exp 
 }
