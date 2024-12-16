df8d3d5eb94d03e32febd9fea58af78526aa4e84
Deka Aditra Erlangga <150640353+Dxdeka9@users.noreply.github.com>
2024-12-16 07:27:31 +0700

    fix komen reg

--- register.php
+++ register.php
@@ -4,8 +4,8 @@ session_start();
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // Ambil input dari form
-    $nama_lengkap = $conn->real_escape_string($_POST['nama_lengkap']);
-    $email = $conn->real_escape_string($_POST['email']);
+    $nama_lengkap = $conn->real_escape_string($_POST['nama_lengkap']); // Mengambil data form 'nama_lengkap' dan membersihkannya menggunakan real_escape_string untuk mencegah SQL Injection
+    $email = $conn->real_escape_string($_POST['email']); // Mengambil data dari form 'email' dan membersihkannya menggunakan metode real_escape_string untuk mencegah SQL Injection
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT); // enkripsi password
     $role = 'mahasiswa'; // Set role default sebagai 'user = mahasiswa'
 
@@ -19,7 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $_SESSION['message'] = "Error: " . $conn->error; // pesan error dalam sesi
         $_SESSION['message_type'] = "danger"; 
     }
-    header("Location: register.php");
+    header("Location: register.php"); // untuk mengarahkan ke register sebelum ke index
     exit();
 }
 ?>
@@ -28,11 +28,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- tampilan res[onsif -->
     <title>Registrasi</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="styling/register.css">
-    <link rel="stylesheet" href="styling/slideshow.css">
+    <link rel="stylesheet" href="styling/register.css"> <!-- untuk style register -->
+    <link rel="stylesheet" href="styling/slideshow.css"> <!-- untuk style slideshow image -->
 </head>
 <body>
     <!-- Slideshow Background -->
@@ -61,7 +61,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             <?php
             //notif pesan
             $message_type = $_SESSION['message_type']; // Menyimpan tipe pesan ke variabel dan menghapus pesan dari sesi
-            unset($_SESSION['message'], $_SESSION['message_type']);
+            unset($_SESSION['message'], $_SESSION['message_type']); // Menghapus pesan dari sesi setelah ditampilkan
             ?>
             <!-- Redirect ke index.php setelah 3 detik jika registrasi berhasil -->
             <?php if ($message_type == "success"): ?> <!-- Jika tipe pesan adalah "success" -->
@@ -93,6 +93,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         <a href="index.php">Sudah punya akun? Login di sini</a> <!-- Link untuk login -->
     </div>
 
-    <script src="slideshow.js"></script>
+    <script src="slideshow.js"></script> <!-- script bagian slideshow -->
 </body>
 </html>
